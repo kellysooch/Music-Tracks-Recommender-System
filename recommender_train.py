9ea5e821dbad6eb143ea51ee861c3803df46bf81
Kelly Sooch <kellysooch@127.0.0.1>
2019-05-02 15:28:39 -0400

    updated recommender_train.py

--- recommender_train.py
+++ recommender_train.py
@@ -23,7 +23,7 @@ def main(spark, data_file, model_file):
     train = spark.read.parquet(data_file)
     
     indexer_user = StringIndexer(inputCol="user_id", outputCol="user", handleInvalid="skip")
-    indexer_item = StringIndexer(inputCol="track_id", outputCol="item", handleInvalid="skip")
+#     indexer_item = StringIndexer(inputCol="track_id", outputCol="item", handleInvalid="skip")
     
     als = ALS(userCol="user", itemCol="item", ratingCol="count")
     pipeline = Pipeline(stages = [indexer_user, indexer_item, als])
