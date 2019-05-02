cec26dbd70edf34574596100bf1ee872f27453cd
Kelly Sooch <kellysooch@127.0.0.1>
2019-05-02 13:54:31 -0400

    added setup_data.py file

---
+++ setup_data.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# We need sys to get the command line arguments
+import sys
+
+from pyspark.ml.feature import StringIndexer
+from pyspark.sql import SparkSession
+from pyspark.ml import Pipeline
+
+def main(spark, data_file, new_data_file):
+    '''
+    Parameters
+    ----------
+    spark : SparkSession object
+    data_file : string, path to the parquet file to load
+    model_file : string, path to store the serialized model file
+    '''
+
+    # Load the parquet file
+    train = spark.read.parquet(data_file)
+    
+    indexer_user = StringIndexer(inputCol="user_id", outputCol="user", handleInvalid="skip")
+    indexer_item = StringIndexer(inputCol="track_id", outputCol="item", handleInvalid="skip")
+    pipeline = Pipeline(stages=[indexer_user, indexer_item])
+    transformed_train = pipeline.fit(train).transform(train)
+    transformed_train.save(new_data_file)
+    
+if __name__ == "__main__":
+
+    # Create the spark session object
+    spark = SparkSession.builder.appName('setup_data').getOrCreate()
+
+    # Get the filename from the command line
+    data_file = sys.argv[1]
+
+    # And the location to store the trained model
+    model_file = sys.argv[2]
+
+    # Call our main routine
+    main(spark, data_file, new_data_file)
