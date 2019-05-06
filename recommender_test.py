8c379f5aeee837bc8d8659a01902256cf71c975f
Kelly Sooch <kellysooch@127.0.0.1>
2019-05-05 23:46:04 -0400

    updated recommender_test.py

--- recommender_test.py
+++ recommender_test.py
@@ -30,7 +30,7 @@ def main(spark, model_file, test_file):
     model = PipelineModel.load(model_file)
     
     test_transformed = model.transform(test)
-    predictionAndLabels = test_transformed.select(["prediction", "count"]).rdd.map(lambda row: (row.prediction, row.count)).take(10)
+    predictionAndLabels = test_transformed.select(["prediction", "count"]).rdd.map(lambda row: (row.prediction, row[2])).take(10)
     print(predictionAndLabels)
 #     rdd = sc.parallelize([[1.0], [0.0]])
 #     metrics = RankingMetrics(rdd)
