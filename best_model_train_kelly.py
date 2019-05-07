2f35eeb9cdaed00251375d632ad16707e9db33ab
Kelly Sooch <kellysooch@127.0.0.1>
2019-05-07 15:53:09 -0400

    fixed error on best_model_train_kelly

--- best_model_train_kelly.py
+++ best_model_train_kelly.py
@@ -35,7 +35,7 @@ def main(spark, train_data_file, rank_val, reg, alpha_val, user_indexer_model, i
     
     pipeline = Pipeline(stages = [indexer_user, indexer_item, als])
     train = indexer_user.transform(train)
-    train = inexer_item.transform(train)
+    train = indexer_item.transform(train)
     model = als.fit(train)
     indexer_user.save(user_indexer_model)
     indexer_item.save(item_indexer_model)
