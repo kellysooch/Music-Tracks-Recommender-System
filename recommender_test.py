a628bf38c4e602f622db9c6346f280ee335145c7
Kelly Sooch <kellysooch@127.0.0.1>
2019-05-08 11:22:28 -0400

    updated recommender_test

--- recommender_test.py
+++ recommender_test.py
@@ -40,17 +40,17 @@ def main(spark, user_indexer_model, item_indexer_model, model_file, test_file):
     print("select users")
     user_recs = model.recommendForUserSubset(user_subset, 500)
     
-    top5 = user_recs.select("user", col("recommendations.item").alias("item")).rdd.map(lambda r: (r.user, r.item))
+    top5 = user_recs.select("user", col("recommendations.item").alias("item"))
 #     top5 = top5.repartition(2000)
     print("select rdd")
     
-    relevant_docs = test.groupBy('user').agg(F.collect_list('item').alias('item')).rdd.map(lambda r: (r.user, r.item))
+    relevant_docs = test.groupBy('user').agg(F.collect_list('item').alias('item'))
 #     test_select = test_transformed.select(col("user"), col("item"), col("count").alias("prediction"))
 #     ratingsTuple = test_select.rdd.map(lambda r: (r.user, [r.prediction])).reduceByKey(lambda p, q: p+q)
     print("made label tuple")
 #     predictionAndLabels = predictions.join(ratingsTuple).map(lambda tup: tup[1])
-    predictionAndLabels = top5.join(relevant_docs).map(lambda tup: tup[1])
-#     print(predictionAndLabels.take(10))
+    predictionAndLabels = top5.join(relevant_docs, "inner").map(lambda tup: tup[1])
+    print(predictionAndLabels.take(10))
 #     predictionAndLabels = predictionAndLabels.repartition(2000)
     print("joined predictions and counts")
 
