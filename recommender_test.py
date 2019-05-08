4990a2a537d45b8d392766fb9f646e349a4d4fbc
Kelly Sooch <kellysooch@127.0.0.1>
2019-05-08 11:46:13 -0400

    updated recommender_test

--- recommender_test.py
+++ recommender_test.py
@@ -26,7 +26,7 @@ def main(spark, user_indexer_model, item_indexer_model, model_file, test_file):
     # Load the parquet file
     test = spark.read.parquet(test_file)
     print("read file")
-    test = test.sample(withReplacement = False, fraction = 0.01)
+#     test = test.sample(withReplacement = False, fraction = 0.01)
     print("sample file")
     user_index = StringIndexerModel.load(user_indexer_model)
     item_index = StringIndexerModel.load(item_indexer_model)
@@ -50,7 +50,7 @@ def main(spark, user_indexer_model, item_indexer_model, model_file, test_file):
     print("made label tuple")
 #     predictionAndLabels = predictions.join(ratingsTuple).map(lambda tup: tup[1])
     predictionAndLabels = top5.join(relevant_docs,["user"], "inner").rdd.map(lambda tup: (tup[1], tup[2]))
-    print(predictionAndLabels.take(10))
+#     print(predictionAndLabels.take(10))
 #     predictionAndLabels = predictionAndLabels.repartition(2000)
     print("joined predictions and counts")
 
