0665749757df573a27fb67e46a4997aaa34654e9
Kelly Sooch <kellysooch@127.0.0.1>
2019-05-08 22:13:43 -0400

    updated recommender_train to use ranking metrics

--- recommender_train.py
+++ recommender_train.py
@@ -31,39 +31,43 @@ def main(spark, train_data_file, val_data_file):
     indexer_item = StringIndexer(inputCol="track_id", outputCol="item",
     handleInvalid="keep").fit(train)
     
-    rank = [1, 5, 10] #default is 10
+    rank = [10, 20, 30] #default is 10
     regularization = [ .01, .1, 1] #default is 1
-    alpha = [ .01, .1, 1] #default is 1
+    alpha = [ .1, .5, 1] #default is 1
     
     train = indexer_user.transform(train)
     train = indexer_item.transform(train)
-    val = indexer_user.transform(val)
-    val = indexer_item.transform(val)
+#     val = indexer_user.transform(val)
+#     val = indexer_item.transform(val)
     
     rank_list = []
     reg_list = []
     alpha_list = []
-    rmses = []
+    precisions = []
 
     for i in rank:
         for j in regularization:
             for k in alpha:
-                als = ALS(userCol = 'user', itemCol = 'item', implicitPrefs =
-                          True, ratingCol = 'count', rank = i, regParam = j, alpha = k)
-                output = als.fit(train)
-                predictions = output.transform(val)
-                evaluator = RegressionEvaluator(metricName="rmse", labelCol="count",predictionCol="prediction")
-                rmse = evaluator.evaluate(predictions)
+                als = ALS(userCol = 'user', itemCol = 'item', implicitPrefs = True, 
+                          ratingCol = 'count', rank = i, regParam = j, alpha = k)
+                model = als.fit(train)
+                subset = val.select('user').distinct()
+                predictions = model.predictForUserSubset(subset, 50)
+                predictions = predictions.select("user", col("recommendations.item").alias("item")).sort('user')
+                val = val.sort('user')
+                predictionAndLabels = predictions.join(val,["user"], "inner").rdd.map(lambda tup: (tup[1], tup[2]))
+                metrics = RankingMetrics(predictionAndLabels)
+                precision = metrics.precisionAt(500)
                 rank_list.append(i)
                 reg_list.append(j)
                 alpha_list.append(k)
-                rmses.append(rmse)
+                precisions.append(precision)
     
     print(rank_list)
     print(reg_list)
     print(alpha_list)
     print(rmses)
-    print('Min RMSE value: %f' %min(rmses))
+    print('Min precision value: %f' %min(precisions))
     ind = np.argmin(rmses)
     print('Rank: %f' %rank_list[ind])
     print('Reg: %f' %reg_list[ind])
