3fd3c3a93aec51711058d1c8dc18ac27f3a60187
Kelly Sooch <kellysooch@127.0.0.1>
2019-05-10 16:04:59 -0400

    updated recommender_test

--- recommender_test.py
+++ recommender_test.py
@@ -25,9 +25,9 @@ def main(spark, model_file, test_file):
 
     # Load the parquet file
     test = spark.read.parquet(test_file)
-    test = test.sort('user', ascending=False)
+    test = test.sort('user')
     test.createOrReplaceTempView('test_table')
-    test = spark.sql('SELECT * FROM test_table LIMIT 50000')
+    test = spark.sql('SELECT * FROM test_table LIMIT 30000')
 #     test = test.sample(withReplacement = False, fraction = 0.4)
     model = ALSModel.load(model_file)
     
@@ -35,7 +35,7 @@ def main(spark, model_file, test_file):
     user_subset = model.recommendForUserSubset(user_subset, 500)
     
     user_subset = user_subset.select("user", col("recommendations.item").alias("item"))
-    user_subset = user_subset.sort('user', ascending=False)
+    user_subset = user_subset.sort('user')
     print("sort user")
     predictionAndLabels = user_subset.join(test,["user"], "inner").rdd.map(lambda tup: (tup[1], tup[2]))
     print("joined predictions and counts")
